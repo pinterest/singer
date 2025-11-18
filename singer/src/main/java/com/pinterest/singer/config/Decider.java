c92d05558a27542da96181fb10f33964bca3061e
Jesus Zuniga <jesuszuniga@pinterest.com>
2025-11-18 11:31:01 -0800

    make changes in decider accessed metric code to add decider name as suffix

--- singer/src/main/java/com/pinterest/singer/config/Decider.java
+++ singer/src/main/java/com/pinterest/singer/config/Decider.java
@@ -243,6 +243,9 @@ public class Decider {
       OpenTsdbMetricConverter.gauge(
           SingerMetrics.DECIDER_ACCESSED, 1,
           "name=" + deciderName);
+
+      OpenTsdbMetricConverter.gauge(
+          SingerMetrics.DECIDER_ACCESSED + "." + deciderName, 1);
       if (mDeciderMap.containsKey(deciderName)) {
         return mDeciderMap.get(deciderName);
       }
